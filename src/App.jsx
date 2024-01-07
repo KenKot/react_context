dea4205864ef09246a5285a82d6727e8341be779
KenKot <kennykotten@gmail.com>
2024-01-07 01:07:34 -0800

    sync

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import Navbar from "./components/Navbar";
+import Booklist from "./components/Booklist";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Booklist />
+      <h1>hello</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+export default function Navbar() {
+  return (
+    <navbar>
+      <h1>Context App</h1>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </navbar>
+  );
+}
